b8b8105ac756ef9fe904601fe9b0e91080a6ed7e
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-01 23:15:01 +0100

    add first unit test

--- src/main/kotlin/uk/co/jpawlak/beltmatic/Main.kt
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/Main.kt
@@ -3,3 +3,5 @@ package uk.co.jpawlak.beltmatic
 fun main() {
     println("Hello World!")
 }
+
+fun blah() = 3
---
+++ src/test/kotlin/uk/co/jpawlak/beltmatic/MainKtTest.kt
@@ -0,0 +1,13 @@
+package uk.co.jpawlak.beltmatic
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+
+    @Test
+    fun test() {
+        assertEquals(3, blah())
+    }
+
+}
