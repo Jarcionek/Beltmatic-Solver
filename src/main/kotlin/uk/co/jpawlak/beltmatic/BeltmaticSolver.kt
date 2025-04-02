2874b9b20d9916a8b2f43a22ce872f0744988c6b
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-03 00:40:31 +0100

    refactor BeltmaticSolver in preparation for a proper algorithm

--- src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
@@ -3,14 +3,35 @@ package uk.co.jpawlak.beltmatic
 class BeltmaticSolver {
 
     fun solve(availableNumbers: List<Int>, targetNumber: Int): String {
-        for (a in availableNumbers) {
-            for (b in availableNumbers) {
-                if (a + b == targetNumber) {
-                    return "$a + $b"
+        val results: MutableMap<Int, Result> = availableNumbers.associateBy({ it }, { Result(it, 0, "$it") }).toMutableMap()
+
+        val newResults: MutableMap<Int, Result> = mutableMapOf()
+        for (a in results.values) {
+            for (b in results.values) {
+                val newResult = join(a, b)
+                if (newResult.targetNumber == targetNumber) {
+                    return newResult.formula
+                }
+                if (!newResults.containsKey(newResult.targetNumber)) {
+                    newResults[newResult.targetNumber] = newResult
                 }
             }
         }
         throw IllegalArgumentException("No formula found")
     }
 
+    private fun join(a: Result, b: Result): Result {
+        return Result(
+            a.targetNumber + b.targetNumber,
+            a.numberOfOperations + b.numberOfOperations,
+            "${a.formula} + ${b.formula}"
+        )
+    }
+
 }
+
+data class Result(
+    val targetNumber: Int,
+    val numberOfOperations: Int,
+    val formula: String,
+)
