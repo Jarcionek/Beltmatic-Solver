7f425290e91d801f1c20e99265a2b4f294570e05
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-03 00:23:10 +0100

    change BeltmaticSolverTest to parameterized test using @ArgumentsSource

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testImplementation("org.openjdk.nashorn:nashorn-core:15.4")
 }
 
--- src/test/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolverTest.kt
+++ src/test/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolverTest.kt
@@ -1,25 +1,39 @@
 package uk.co.jpawlak.beltmatic
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 import javax.script.ScriptEngineManager
 
 class BeltmaticSolverTest {
 
     private val solver = BeltmaticSolver()
 
-    @Test
-    fun `returns 1 operation formula with simple addition`() {
-        val formula = solver.solve(listOf(2, 3, 4), 7)
+    class MathTestArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+            return Stream.of(
+                Arguments.of(listOf(2, 3, 4), 7),
+                Arguments.of(listOf(3, 1, 5), 4),
+                Arguments.of(listOf(10, 5, 2), 12)
+            )
+        }
+    }
+
+    //TODO improve tests names
+    @ParameterizedTest
+    @ArgumentsSource(MathTestArgumentsProvider::class)
+    fun `returns 1 operation formula with simple addition`(availableNumbers: List<Int>, targetNumber: Int) {
+        val formula = solver.solve(availableNumbers, targetNumber)
 
         val result = evaluate(formula)
 
         //TODO user proper testing library
-        assertEquals(7, result)
-        assertTrue(formula == "3 + 4" || formula == "4 + 3")
+        assertEquals(targetNumber, result)
     }
-
 
     @Suppress("MoveVariableDeclarationIntoWhen")
     private fun evaluate(expression: String): Int {
