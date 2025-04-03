76daea25314b5ac3c3dbed05cf9eebc5b914c168
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-03 19:19:29 +0100

    rename Result to AvailableNumber

--- src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
@@ -3,16 +3,20 @@ package uk.co.jpawlak.beltmatic
 class BeltmaticSolver {
 
     fun solve(availableNumbers: List<Int>, targetNumber: Int): String {
-        val results: MutableMap<Int, Result> = availableNumbers.associateBy({ it }, { Result(it, 0, "$it") }).toMutableMap()
+        val results: MutableMap<Int, AvailableNumber> = availableNumbers.associateBy({ it }, { AvailableNumber(
+            it,
+            "$it",
+            0
+        ) }).toMutableMap()
 
         for (i in 1..2) { //TODO extract to MAX_OPERATIONS constant; update exception message
-            val newResults: MutableMap<Int, Result> = calculateNewResults(results)
+            val newResults: MutableMap<Int, AvailableNumber> = calculateNewResults(results)
             if (newResults.containsKey(targetNumber)) {
                 return newResults[targetNumber]!!.formula
             }
             for (newResult in newResults) {
                 val oldResult = results[newResult.key]
-                if (oldResult == null || oldResult.numberOfOperations > newResult.value.numberOfOperations) {
+                if (oldResult == null || oldResult.formulaOperationsCount > newResult.value.formulaOperationsCount) {
                     results[newResult.key] = newResult.value
                 }
             }
@@ -21,31 +25,30 @@ class BeltmaticSolver {
         throw IllegalArgumentException("No formula found")
     }
 
-    private fun calculateNewResults(results: Map<Int, Result>): MutableMap<Int, Result> {
-        val newResults: MutableMap<Int, Result> =  mutableMapOf()
+    private fun calculateNewResults(results: Map<Int, AvailableNumber>): MutableMap<Int, AvailableNumber> {
+        val newResults: MutableMap<Int, AvailableNumber> =  mutableMapOf()
         for (a in results.values) {
             for (b in results.values) {
                 val newResult = join(a, b)
-                newResults[newResult.targetNumber] = newResult
+                newResults[newResult.number] = newResult
                 //TODO when inserting into the map, we need to pick the one with fewest operations - this code is already in the function above
             }
         }
         return newResults
     }
 
-    private fun join(a: Result, b: Result): Result {
-        return Result(
-            a.targetNumber + b.targetNumber,
-            a.numberOfOperations + b.numberOfOperations,
-            "${a.formula} + ${b.formula}" //TODO will need parenthesis here
+    private fun join(a: AvailableNumber, b: AvailableNumber): AvailableNumber {
+        return AvailableNumber(
+            a.number + b.number,
+            "${a.formula} + ${b.formula}", //TODO will need parenthesis here
+            a.formulaOperationsCount + b.formulaOperationsCount
         )
     }
 
 }
 
-//TODO rename it - how to call it? AvailableNumber?
-data class Result(
-    val targetNumber: Int,
-    val numberOfOperations: Int,
+data class AvailableNumber(
+    val number: Int,
     val formula: String,
+    val formulaOperationsCount: Int,
 )
