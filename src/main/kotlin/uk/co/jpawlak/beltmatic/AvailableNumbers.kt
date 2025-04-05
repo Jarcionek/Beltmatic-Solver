6b762f52e3c5eb10f6e7ee478584f9a829b30228
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-05 17:38:22 +0100

    extract map of all AvailableNumbers into its own class

---
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/AvailableNumbers.kt
@@ -0,0 +1,28 @@
+package uk.co.jpawlak.beltmatic
+
+class AvailableNumbers(initiallyAvailableNumbers: List<Int>) {
+
+    private val allAvailableNumbers: MutableMap<Int, AvailableNumber> = initiallyAvailableNumbers.associateBy(
+        { it },
+        { AvailableNumber(it, "$it", 0) }
+    ).toMutableMap()
+
+    /**
+     * If [newNumber] has smaller operation count than the already existing [AvailableNumber]
+     * (or if no such number already exists),
+     * stores the [newNumber] in [AvailableNumbers].
+     */
+    fun addIfBetter(newNumber: AvailableNumber) {
+        val oldNumber = allAvailableNumbers[newNumber.number]
+        if (oldNumber == null || newNumber.formulaOperationsCount < oldNumber.formulaOperationsCount) {
+            allAvailableNumbers[newNumber.number] = newNumber
+        }
+    }
+
+    fun getAll() = allAvailableNumbers.values.toList()
+
+    fun get(targetNumber: Int): AvailableNumber? {
+        return allAvailableNumbers[targetNumber]
+    }
+
+}
--- src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolver.kt
@@ -13,61 +13,43 @@ class BeltmaticSolver(
     private val availableNumberCalculator: AvailableNumberCalculator = AvailableNumberCalculator()
 ) {
 
+    private lateinit var allAvailableNumbers: AvailableNumbers
+
     fun solve(initiallyAvailableNumbers: List<Int>, targetNumber: Int): String {
-        val allAvailableNumbers: MutableMap<Int, AvailableNumber> = initiallyAvailableNumbers.associateBy(
-            { it },
-            { AvailableNumber(it, "$it", 0) }
-        ).toMutableMap()
+        allAvailableNumbers = AvailableNumbers(initiallyAvailableNumbers)
 
         for (i in 1..MAX_ITERATIONS) {
 
-            calculateNewAvailableNumbers(allAvailableNumbers)
-                .onEach {
-                    //TODO with longer formulas, we should probably finish the iteration to find the best formula, but no test is catching this yet
-                    if (it.key == targetNumber) {
-                        return it.value.formula
-                    }
-                }
-                .forEach { addIfBetter(allAvailableNumbers, it.value) }
+            calculateNewAvailableNumbers(allAvailableNumbers.getAll())
+                .forEach { allAvailableNumbers.addIfBetter(it) }
 
-//            val formula: String? = allAvailableNumbers[targetNumber]?.formula
-//            if (formula != null) {
-//                return formula
-//            }
+            //TODO suboptimal - waiting for the iteration to finish, even if the target number was already found
+            val formula: String? = allAvailableNumbers.get(targetNumber)?.formula
+            if (formula != null) {
+                return formula
+            }
         }
 
         throw IllegalArgumentException("Could not find a formula to get $targetNumber using no more than $MAX_ITERATIONS iterations")
     }
 
-    private fun calculateNewAvailableNumbers(availableNumbers: Map<Int, AvailableNumber>): MutableMap<Int, AvailableNumber> {
-        val newNumbers: MutableMap<Int, AvailableNumber> = mutableMapOf()
-        for (a in availableNumbers.values) {
-            for (b in availableNumbers.values) {
-                addIfBetter(newNumbers, availableNumberCalculator.add(a, b))
-                addIfBetter(newNumbers, availableNumberCalculator.subtract(a, b))
-                addIfBetter(newNumbers, availableNumberCalculator.multiply(a, b))
+    private fun calculateNewAvailableNumbers(availableNumbers: List<AvailableNumber>): Sequence<AvailableNumber> {
+        return availableNumbers.asSequence().flatMap { a ->
+            availableNumbers.asSequence().flatMap { b ->
+                val newNumbers: MutableList<AvailableNumber> = mutableListOf()
+                //TODO optimize - even if the target number was found, this will continue the whole iteration
+                newNumbers.add(availableNumberCalculator.add(a, b))
+                newNumbers.add(availableNumberCalculator.subtract(a, b))
+                newNumbers.add(availableNumberCalculator.multiply(a, b))
                 if (b.number in 2..3) { //TODO without this restriction it runs out of heap
                     if (a.number < 65536) { // this is 2^31, square it, and we get MAX INT; for cube the limit is around 1625; for ^4 the limit is 256
-                        addIfBetter(newNumbers, availableNumberCalculator.power(a, b))
+                        newNumbers.add(availableNumberCalculator.power(a, b))
                     }
                 }
+                newNumbers.asSequence()
             }
         }
-        return newNumbers
     }
 
-    /**
-     * If [newNumber] has smaller operation count than the [AvailableNumber] in [availableNumbers],
-     * puts the [newNumber] into the [Map].
-     */
-    private fun addIfBetter(
-        availableNumbers: MutableMap<Int, AvailableNumber>,
-        newNumber: AvailableNumber
-    ) {
-        val oldNumber = availableNumbers[newNumber.number]
-        if (oldNumber == null || newNumber.formulaOperationsCount < oldNumber.formulaOperationsCount) {
-            availableNumbers[newNumber.number] = newNumber
-        }
-    }
 }
 
