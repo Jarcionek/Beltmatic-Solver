393a2fdb0cfb2754205bb4066155394e38e40da1
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-16 22:18:49 +0100

    change ExpressionEvaluator to execute exponentiations right to left

--- src/test/kotlin/uk/co/jpawlak/beltmatic/ExpressionEvaluator.kt
+++ src/test/kotlin/uk/co/jpawlak/beltmatic/ExpressionEvaluator.kt
@@ -3,19 +3,25 @@ package uk.co.jpawlak.beltmatic
 import com.fathzer.soft.javaluator.BracketPair
 import com.fathzer.soft.javaluator.DoubleEvaluator
 import com.fathzer.soft.javaluator.DoubleEvaluator.DIVIDE
+import com.fathzer.soft.javaluator.DoubleEvaluator.EXPONENT
 import com.fathzer.soft.javaluator.Operator
 import com.fathzer.soft.javaluator.Parameters
-
+import kotlin.math.pow
 
 private const val SCIENTIFIC_NOTATION_NOT_SUPPORTED = false
 
 object ExpressionEvaluator {
 
+    /**
+     * This is a workaround for a [Javaluator bug](https://github.com/fathzer/javaluator/issues/13).
+     */
+    private val RIGHT_EXPONENT: Operator = Operator(EXPONENT.symbol, EXPONENT.operandCount, Operator.Associativity.RIGHT, EXPONENT.precedence)
+
     private val INTEGER_DIVIDE: Operator = Operator(DIVIDE.symbol, DIVIDE.operandCount, DIVIDE.associativity, DIVIDE.precedence)
 
     private val evaluator: DoubleEvaluator = object : DoubleEvaluator(
         Parameters().apply {
-            add(EXPONENT)
+            add(RIGHT_EXPONENT)
             add(MULTIPLY)
             add(INTEGER_DIVIDE)
             add(PLUS)
@@ -30,11 +36,13 @@ object ExpressionEvaluator {
             operands: MutableIterator<Double>?,
             evaluationContext: Any?
         ): Double {
-            return if (operator == INTEGER_DIVIDE) {
-                (operands!!.next().toInt() / operands.next().toInt()).toDouble()
-            } else {
-                super.evaluate(operator, operands, evaluationContext)
+            if (operator == RIGHT_EXPONENT) {
+                return operands!!.next().pow(operands.next())
             }
+            if (operator == INTEGER_DIVIDE) {
+                return (operands!!.next().toInt() / operands.next().toInt()).toDouble()
+            }
+            return super.evaluate(operator, operands, evaluationContext)
         }
 
     }
--- src/test/kotlin/uk/co/jpawlak/beltmatic/ExpressionEvaluatorTest.kt
+++ src/test/kotlin/uk/co/jpawlak/beltmatic/ExpressionEvaluatorTest.kt
@@ -1,5 +1,8 @@
 package uk.co.jpawlak.beltmatic
 
+import com.fathzer.soft.javaluator.DoubleEvaluator
+import com.fathzer.soft.javaluator.Operator.Associativity.LEFT
+import com.fathzer.soft.javaluator.Operator.Associativity.RIGHT
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -39,5 +42,20 @@ class ExpressionEvaluatorTest {
         val result = ExpressionEvaluator.evaluate(expression)
 
         assertEquals(6, result)
+    }
+
+    /**
+     * `4^3^2 = 4^(3^2) = 4^9 = 262,144`, however, [DoubleEvaluator.EXPONENT] is incorrectly defined
+     * as [LEFT] associativity, instead or [RIGHT], which results in `(4^3)^2 = 64^2 = 4096`.
+     *
+     * See [Javaluator bug](https://github.com/fathzer/javaluator/issues/13).
+     */
+    @Test
+    fun `evaluates multiple exponentiations right to left`() {
+        val expression = "4^3^2"
+
+        val result = ExpressionEvaluator.evaluate(expression)
+
+        assertEquals(262144, result)
     }
 }
