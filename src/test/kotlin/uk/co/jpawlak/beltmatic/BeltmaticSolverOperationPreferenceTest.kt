3c839a8cd58b867f4fa6c750a36ffeb2c4be8734
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-18 14:40:28 +0100

    merge BeltmaticSolverPerformanceTest into BeltmaticSolverOperationPreferenceTest

--- src/test/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolverOperationPreferenceTest.kt
+++ src/test/kotlin/uk/co/jpawlak/beltmatic/BeltmaticSolverOperationPreferenceTest.kt
@@ -16,11 +16,39 @@ import java.util.concurrent.TimeUnit
  *
  * The smallest operation count is still the most important factor, as we want to build in the game as few machines
  * as necessary.
+ *
+ * Note that these tests use a big pool of initial numbers which makes them rather slow.
  */
 @Timeout(value = 30, unit = TimeUnit.SECONDS)
 class BeltmaticSolverOperationPreferenceTest {
 
     private val solver = BeltmaticSolverFactory.create()
+
+    @Test
+    fun `8339 is solved with multiplications and additions only (and no parentheses)`() {
+        val availableNumbers: List<Int> = (1..9) + (11..24)
+        val targetNumber = 8339
+        val operationsLimit = 3
+
+        FormulaVerifier.verify("2 ^ 13 + 7 * 21", availableNumbers, targetNumber, operationsLimit) // BAD
+        FormulaVerifier.verify("18 * 21 * 22 + 23", availableNumbers, targetNumber, operationsLimit) // GOOD
+
+        val formula = solver.solve(availableNumbers, targetNumber)
+        val result = ExpressionEvaluator.evaluate(formula)
+
+        assertEquals(targetNumber, result, "The result of $formula is not $targetNumber")
+        assertEquals(operationsLimit, FormulaVerifier.operationCount(formula), "Unexpected number of operations: $formula")
+
+        assertTrue(
+            !formula.contains('^') && !formula.contains('-') && !formula.contains('/'),
+            "Formula should only contain multiplications and additions: $formula"
+        )
+
+        assertTrue(
+            !formula.contains('(') && !formula.contains(')'),
+            "Formula should not contain any parentheses: $formula"
+        )
+    }
 
     @Test
     fun `3955 is solved with multiplications and additions only`() {
