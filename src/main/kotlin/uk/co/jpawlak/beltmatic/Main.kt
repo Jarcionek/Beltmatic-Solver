b029b48a95af2f05140aef7be4f56274ef2f570f
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2025-04-18 17:38:34 +0100

    add timer to Main

--- src/main/kotlin/uk/co/jpawlak/beltmatic/Main.kt
+++ src/main/kotlin/uk/co/jpawlak/beltmatic/Main.kt
@@ -1,5 +1,8 @@
 package uk.co.jpawlak.beltmatic
 
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit.SECONDS
+
 object Config {
     /**
      * The solver will never create numbers bigger than this limit, or lower than negative value of this limit.
@@ -28,7 +31,10 @@ fun main() {
 
     val availableNumbers = ((1..9) + (11..24)).toList() // 25 26 27 28 29 30 32 33 34 35 36 37 38 39 39 40 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 60 61 62 64 65 66 68 69 70 71 72 73 74 76 77 78 79 81 83 84 87 89 90 91 92 93 94 96 98 101 103 104 105 106 111 114 117 124 126 130 135 140 ... 991
 
+    val start = LocalDateTime.now()
     val result = BeltmaticSolverFactory.create().solve(availableNumbers, 8339)
+    val end = LocalDateTime.now()
 
     println(result)
+    println("Solved in ${start.until(end, SECONDS)} seconds.")
 }
